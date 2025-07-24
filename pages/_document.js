da30fe92e396e35c32719e060aa99dfcc3ce43d0
Peter Dimo <pdimakidis@gmail.com>
2025-07-24 12:07:01 +1000

    fix: Resolve ReferenceError: process is not defined in _document.js
    - Refactor GTM script in pages/_document.js to ensure NEXT_PUBLIC_GTM_IDS is processed server-side.
    - Prevents 'process is not defined' error in browser by injecting GTM IDs directly into HTML.

--- pages/_document.js
+++ pages/_document.js
@@ -11,19 +11,24 @@ export default function Document() {
             __html: `
               window.dataLayer = window.dataLayer || [];
               window.dataLayer.push({ 'visitorType': 'New' });
-              try {
-                const gtmIds = JSON.parse(process.env.NEXT_PUBLIC_GTM_IDS || '[]');
-                if (Array.isArray(gtmIds)) {
-                  gtmIds.forEach(gtmId => {
-                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                    })(window,document,'script','dataLayer',gtmId);
-                  });
-                }
-              } catch (e) {
-                console.error('Error parsing NEXT_PUBLIC_GTM_IDS:', e);
+              ${
+                (() => {
+                  try {
+                    const gtmIds = JSON.parse(process.env.NEXT_PUBLIC_GTM_IDS || '[]');
+                    if (Array.isArray(gtmIds)) {
+                      return gtmIds.map(gtmId => `
+                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','${gtmId}');
+                      `).join('');
+                    }
+                  } catch (e) {
+                    console.error('Error parsing NEXT_PUBLIC_GTM_IDS:', e);
+                  }
+                  return '';
+                })()
               }
             `,
           }}
